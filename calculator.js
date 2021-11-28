7efc1432a71bd723c61ad0cbf903234795f52cd7
khanm294 <mehakrafi2000@gmail.com>
2021-11-27 23:47:25 -0500

    adding more java functionality on how to add nums

--- calculator.js
+++ calculator.js
@@ -1,7 +1,30 @@
+let value1 = '';
+let value2 = 0;
 function getValue(num) {
     let x = document.getElementById("screen");
+    if (num === '+') {
+        x.innerHTML = '';
+        value2 = AddNums(Number(value1), value2);
+        value1 = '';
+    }
+    else if (num === '=') {
+        x.innerHTML = String(AddNums(Number(value1), value2));
+    }
+    else {
+    value1 = value1 + String(num);
+    console.log(value1);
     let y = document.createTextNode(num);
     x.appendChild(y);
+    }
+    
+}
+
+function AddNums(value1, value2) {
+    value2 = value1 + value2;
+    console.log(value2);
+    return value2;
+    
+    
 }
 
 
